1b721b1e237617041b2eb4bbebd4c7eaa0285efb
jjy <galid1@naver.com>
2019-01-29 16:59:00 +0900

    init

---
+++ src/main/java/com/beta/surveyweb/SurveyController.java
@@ -0,0 +1,13 @@
+package com.beta.surveyweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SurveyController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/beta/surveyweb/SurveyWebApplication.java
@@ -0,0 +1,14 @@
+package com.beta.surveyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyWebApplication.class, args);
+    }
+
+}
+
